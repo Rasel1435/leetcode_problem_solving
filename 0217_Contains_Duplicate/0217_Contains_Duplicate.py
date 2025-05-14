5321fc209ce9381f9db6cc618a4d12d2ff940074
rasel-scs <rasel@shortcircuit.science>
2025-05-14 20:55:49 +0600

    0217 updated

--- 0217_Contains_Duplicate/0217_Contains_Duplicate.py
+++ 0217_Contains_Duplicate/0217_Contains_Duplicate.py
@@ -15,13 +15,38 @@ Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 """
+
+
+nums = [1,2,3,4,1]
 class Solution:
     def containsDuplicate(self, nums):
-
         busket = set()
-
+        
         for n in nums:
             if n in busket:
                 return True
             busket.add(n)
         return False
+    
+sol = Solution()
+result = sol.containsDuplicate(nums)
+print(result)
+
+
+
+"""
+Step-by-step: Breakdown this code
+
+busket = set() # empty set
+
+Loop over: 1 → not in set → add → busket = {1}
+
+Next: 2 → not in set → add → busket = {1, 2} 
+
+Next: 3 → not in set → add → busket = {1, 2, 3}
+
+Next: 4 → not in set → add → busket = {1, 2, 3, 4}
+
+Next: 1 → already in set → return True
+
+"""
