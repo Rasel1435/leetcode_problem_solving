855d3192d4dc89e85bc7507f857ceae820a2c074
rasel-scs <rasel@shortcircuit.science>
2025-05-15 08:07:12 +0600

    Fix: minore chnaged & updated

--- 0242_Valid_Anagram/0242_Valid_Anagram.py
+++ 0242_Valid_Anagram/0242_Valid_Anagram.py
@@ -14,6 +14,7 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 """
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -26,21 +27,40 @@ class Solution:
             
         return countS == countT
     
-    
-# another way 
-from collections import Counter
-class Soluation:
-    def __init__(self, s: str, t: str) -> None:
-        self.s = s
-        self.t = t
-    def isAnagram(self) -> bool:
-        return sorted(self.s) == sorted(self.t)
-        return Counter(self.s) == Counter(self.t)
-        
-if __name__ == '__main__':
-    s = input()
-    print(f'S: {s} ')
-    t = input()
-    print(f'T: {t} ')
-    sol = Soluation(s, t)
-    print(sol.isAnagram())
+sol = Solution()
+result = sol.isAnagram("anagram", "nagaram")
+print(result)
+
+
+"""
+Loop Iteration
+i = 0
+s[0] = 'a'
+t[0] = 'n'
+
+countS['a'] = 1 + countS.get('a', 0) → 1 + 0 = 1
+countT['n'] = 1 + countT.get('n', 0) → 1 + 0 = 1
+
+countS = {'a': 1}, countT = {'n': 1}
+<--------------------------->
+
+i = 1
+s[1] = 'n'
+t[1] = 'a'
+countS['n'] = 1 + countS.get('n', 0) → 1 + 0 = 1
+countT['a'] = 1 + countT.get('a', 0) → 1 + 0 = 1
+countS = {'a': 1, 'n': 1}, countT = {'n': 1, 'a': 1}
+<--------------------------->
+
+i = 2
+s[2] = 'a'
+t[2] = 'g'
+
+countS['a'] = 1 + countS.get('a', 1) → 1 + 1 = 2
+countT['g'] = 1 + countT.get('g', 0) → 1 + 0 = 1
+countS = {'a': 2, 'n': 1}, countT = {'n': 1, 'a': 1, 'g': 1}
+<--------------------------->
+Continue...
+
+
+"""
